f42dc2b13e535b7152a62c1fa4628bd18a9e5b66
Ilya <ilya.gorobievskij@bk.ru>
2020-04-13 08:14:31 +0300

    OPEN_BETA_V_0.2

--- data/player.py
+++ data/player.py
@@ -8,10 +8,10 @@ class Player(SqlAlchemyBase):
     name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
     money = Column(Integer, default=0, nullable=False)
-    job = Column(Integer, ForeignKey('jobs.id'), nullable=True)
-    home_id = Column(Integer, ForeignKey('homes.id'), nullable=True)
-    car_id = Column(Integer, ForeignKey('cars.id'), nullable=True)
-    animal_id = Column(Integer, ForeignKey('animals.id'), nullable=True)
+    job = Column(Integer, ForeignKey('jobs.id'), default=1, nullable=True)
+    home_id = Column(Integer, ForeignKey('homes.id'), default=1, nullable=True)
+    car_id = Column(Integer, ForeignKey('cars.id'), default=1, nullable=True)
+    animal_id = Column(Integer, ForeignKey('animals.id'), default=1, nullable=True)
     created_date = Column(DateTime, default=dt.now, nullable=False)
 
     def __repr__(self):
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ db_session.global_init("db/blogs.sqlite")
 db = db_session.create_session()
 
 
-def sendrer_messages(text, id=int, vk=vk_api.vk_api.VkApiMethod, attachment=None, keyb_json=None):
+def sendrer_messages(text='', id=int, vk=vk_api.vk_api.VkApiMethod, attachment=None, keyb_json=None):
     if attachment is None and keyb_json is None:
         vk.messages.send(user_id=id, random_id=random.randint(0, 2**64), message=text)
     elif attachment is None and keyb_json is not None:
@@ -31,7 +31,7 @@ def create_keyb_json(list_keys):
     return keyb.get_keyboard()
 
 
-def start(text=str, response=list, name=str, from_id=int, vk=vk_api.vk_api.VkApiMethod):
+def start(text, response=list, name=str, from_id=int, vk=vk_api.vk_api.VkApiMethod):
     if (text.startswith('привет!') or text.startswith('привет') or text.startswith('hi!') or
             text.startswith('hi') or text.startswith('hello') or text.startswith('hello!')):
         try:
@@ -40,15 +40,14 @@ def start(text=str, response=list, name=str, from_id=int, vk=vk_api.vk_api.VkApi
             answer = f'Привет, {name}!\nКак поживает {city}?'
         except:
             answer = f'Привет, {name}!'
-        sendrer_messages(answer, from_id, vk, keyb_json=create_keyb_json([['время'], ['игра'],
+        sendrer_messages(answer, from_id, vk, keyb_json=create_keyb_json([['время'], ['игра', 'настройки'],
                                                                           ['убери клавиатуру']]))
 
 
-def what_r_u_say(text=str, id=int, vk=None, upload=VkUpload):
-    if text == '':
-        attachment = upload_photo('static/img/what_a_u_say.png', upload=upload)
-        sendrer_messages(id=id, vk=vk, attachment=attachment)
-        sendrer_messages(id=id, vk=vk, text='Прости я ещё не знаю что это.')
+def what_r_u_say(upload, id=int, vk=None):
+    attachment = upload_photo('static/img/what_a_u_say.png', upload=upload)
+    sendrer_messages(id=id, vk=vk, attachment=attachment)
+    sendrer_messages(id=id, vk=vk, text='Прости я ещё не знаю что это.')
 
 
 def upload_photo(name, upload=VkUpload):
@@ -105,27 +104,33 @@ def game(text, from_id, vk):
         output = create_player(from_id, name=name)
         if not output:
             sendrer_messages(id=from_id, vk=vk,
-                             text='У тебя уже есть аккаунт здесь, для изменения имени есть другая команда!',
-                             keyb_json=create_keyb_json([['/работа', '/дом', '/животное'], ['/магаз', '/статы']]))
+                             text='У тебя уже есть аккаунт здесь, для изменения имени есть другая команда!')
         else:
             sendrer_messages(id=from_id, vk=vk,
-                             text='Имя добавлено!')
+                             text='Имя добавлено!',
+                             keyb_json=create_keyb_json([['/работа', '/дом', '/животное', '/машина'], ['/магаз',
+                                                                                                       'настройки',
+                                                                                                       '/статы'],
+                                                         ['выйти из игры']]))
     if text.startswith('моя фамилия - '):
         surname = text[len('моя фамилия - '):]
         output = create_player(from_id, surname=surname)
         if not output:
             sendrer_messages(id=from_id, vk=vk,
-                             text='У тебя уже есть аккаунт здесь, для изменения фамилии есть другая команда!',
-                             keyb_json=create_keyb_json([['/работа', '/дом', '/животное'], ['/магаз', '/статы']]))
+                             text='У тебя уже есть аккаунт здесь, для изменения фамилии есть другая команда!')
         else:
             sendrer_messages(id=from_id, vk=vk,
-                             text='Имя добавлено!')
+                             text='Имя добавлено!',
+                             keyb_json=create_keyb_json([['/работа', '/дом', '/животное', '/машина'], ['/магаз',
+                                                                                                       'настройки',
+                                                                                                       '/статы'],
+                                                         ['выйти из игры']]))
 
 
 def create_player(id, name=None, surname=None):
     global db
     player = get_player(id)
-    if player[0]:
+    if not player[0]:
         player = Player()
         player.vk_id = id
         if name is not None: player.name = name
@@ -180,24 +185,24 @@ def create_property():
         if _ == 1:
             create_job(names_job[_ - 1], 0)
         else:
-            create_job(names_job[_ - 1], _ ** 2.1)
+            create_job(names_job[_ - 1], round(_ ** 2.1))
 
         if _ == 10:
-            create_home(names_home[_ - 1], 2**64)
+            create_home(names_home[_ - 1], round(2**16))
         if _ == 1:
             create_home(names_job[_ - 1], 0)
-        if (1 < _) and (_ < 11):
-            create_home(names_home[_ - 1], _ ** 2.1)
+        if (1 < _) and (_ < 10):
+            create_home(names_home[_ - 1], round(_ ** 2.1))
 
         if _ == 1:
             create_car(names_car[_ - 1], 0)
         else:
-            create_car(names_car[_ - 1], _ ** 2.1)
+            create_car(names_car[_ - 1], round(_ ** 2.1))
 
         if _ == 1:
             create_animal(names_animal[_ - 1], 0)
         else:
-            create_animal(names_animal[_ - 1], _ ** 2.1)
+            create_animal(names_animal[_ - 1], round(_ ** 2.1))
 
 
 def change_data_player(id, name=None, surname=None):
@@ -213,7 +218,7 @@ def change_data_player(id, name=None, surname=None):
 
 def get_player(id):
     global db
-    user = db.query(Player).filter(Player.id == id).first()
+    user = db.query(Player).filter(Player.vk_id == id).first()
     if user:
         return (True, user)
     return (False, None)
@@ -225,133 +230,193 @@ def chek_player(from_id, vk):
     if not resp[0]:
         sendrer_messages(id=from_id, vk=vk,
                          text='Такого игорока нет в списке!')
-        return False, None
-    return True, resp
+        return False
+    return True
 
 
-def commands(text, from_id, vk):
+def commands(text, from_id, vk, upload):
+    global db
     if text == 'статы':
         res = chek_player(from_id, vk)
-        if res[0]:
-            global db
-            user = res[1]
-            job = db.query(Job).filter(Job.id == user.job).first().name
-            home = db.query(Home).filter(Home.id == user.home_id).first().name
-            car = db.query(Car).filter(Car.id == user.car_id).first().name
-            animal = db.query(Animal).filter(Animal.id == user.animal_id).first().name
-            ans = f'<Игрок>\nимя {user.name}\nфамилия {user.last_name}\nкошелёк {user.money}\nработа {job}\nдом {home}\nмашина {car}\nпитомец {animal}\nдата создания аккаунта {user.created_date}'
+        if res:
+            user = db.query(Player).filter(Player.vk_id == from_id).first()
+            job = db.query(Job).filter(Job.id == user.job).first()
+            print(job.name)
+            home = db.query(Home).filter(Home.id == user.home_id).first()
+            print(home.name)
+            car = db.query(Car).filter(Car.id == user.car_id).first()
+            print(car.name)
+            animal = db.query(Animal).filter(Animal.id == user.animal_id).first()
+            print(animal.name)
+            ans = f'<Игрок>\nимя {user.name}\nфамилия {user.last_name}\nкошелёк {user.money}\nработа {job.name}\nдом {home.name}\nмашина {car.name}\nпитомец {animal.name}\nдата создания аккаунта {user.created_date}'
             sendrer_messages(ans, id=from_id, vk=vk)
 
     if text == 'работа':
         res = chek_player(from_id, vk)
-        if res[0]:
-            global db
-            user = res[1]
-            job = db.query(Job).filter(Job.id == user.job).first().name
-            if job == '':
+        if res:
+            user = db.query(Player).filter(Player.vk_id == from_id).first()
+            job = db.query(Job).filter(Job.id == user.job).first()
+            if job.name == '':
                 sendrer_messages(text='ИДИ НА ЗАВОД!!!!!', id=from_id, vk=vk)
             else:
-                sendrer_messages(text=f'Твоя работа: {job}', id=from_id, vk=vk)
+                sendrer_messages(text=f'Твоя работа: {job.name}', id=from_id, vk=vk)
 
     if text == 'дом':
         res = chek_player(from_id, vk)
-        if res[0]:
-            global db
-            user = res[1]
-            home = db.query(Home).filter(Home.id == user.home).first().name
-            if home == '':
+        if res:
+            user = db.query(Player).filter(Player.vk_id == from_id).first()
+            home = db.query(Home).filter(Home.id == user.home_id).first()
+            if home.name == '':
                 sendrer_messages(text='КАКОЙ ДОМ НАФИГ, ТЫ БЕЗДОМНЫЙ!!!!!', id=from_id, vk=vk)
             else:
-                sendrer_messages(text=f'Твоя работа: {home}', id=from_id, vk=vk)
+                sendrer_messages(text=f'Твой дом: {home.name}', id=from_id, vk=vk)
+
+    if text == 'машина':
+        res = chek_player(from_id, vk)
+        if res:
+            user = db.query(Player).filter(Player.vk_id == from_id).first()
+            car = db.query(Car).filter(Car.id == user.car_id).first()
+            if car.name == '':
+                sendrer_messages(text='Нет у тебя машины.', id=from_id, vk=vk)
+            else:
+                sendrer_messages(text=f'Твоя машина: {car.name}', id=from_id, vk=vk)
 
     if text == 'животное':
         res = chek_player(from_id, vk)
-        if res[0]:
-            global db
-            user = res[1]
-            animal = db.query(Animal).filter(Animal.id == user.animal).first().name
-            if animal == '':
+        if res:
+            user = db.query(Player).filter(Player.vk_id == from_id).first()
+            animal = db.query(Animal).filter(Animal.id == user.animal_id).first()
+            if animal.name == '':
                 sendrer_messages(text='Нету у тебя питомца', id=from_id, vk=vk)
             else:
-                sendrer_messages(text=f'Твоя работа: {animal}', id=from_id, vk=vk)
+                sendrer_messages(text=f'Твоя работа: {animal.name}', id=from_id, vk=vk)
 
     if text == 'магаз':
-        global db
 
         ans = ''
 
         ans += 'РАБОТЫ:\n'
         jobs = db.query(Job).filter(Job.id > 1)
         for _ in jobs:
-            ans += (_ + '\n')
+            ans += (_.__repr__() + '\n')
         ans += '\n'
 
         ans += 'ДОМА:\n'
         homes = db.query(Home).filter(Home.id > 1)
         for _ in homes:
-            ans += (_ + '\n')
+            ans += (_.__repr__() + '\n')
         ans += '\n'
 
         ans += 'МАШИНЫ:\n'
         cars = db.query(Car).filter(Car.id > 1)
         for _ in cars:
-            ans += (_ + '\n')
+            ans += (_.__repr__() + '\n')
         ans += '\n'
 
         ans += 'ПИТОМЦЫ:\n'
         animals = db.query(Animal).filter(Animal.id > 1)
         for _ in animals:
-            ans += (_ + '\n')
+            ans += (_.__repr__() + '\n')
         ans += '\n'
 
         ans += 'Чтобы купить что-то, напиши так: /купить [категория] [id]\n'
-        ans += 'Категорию указывать надо в Иминительном падеже заглавными буквами, то есть: РАБОТА, МАШИНА...'
-        ans += 'Да ты должен купить работу\n'
+        ans += 'Категорию указывать надо в Иминительном падеже заглавными буквами, то есть: РАБОТА, МАШИНА...\n'
+        ans += 'Да ты должен купить работу'
 
         sendrer_messages(text=ans, vk=vk, id=from_id)
 
     if text.startswith('купить'):
         resp = text.split()
         category, id = resp[1], resp[2]
-        global db
-        bol, usr = chek_player(from_id, vk)
+        bol = chek_player(from_id, vk)
+        usr = db.query(Player).filter(Player.vk_id == from_id).first()
         if bol:
 
-            if category == 'РАБОТА':
-                usr.job = int(id)
-                sendrer_messages(id=from_id, vk=vk,
-                                 text="Ты устроился на работу\nАванс в размере 20'000 вирт упал на счёт.")
-                usr.money = usr.money + 20000
+            if category == 'работа':
+                try:
+                    usr.job = int(id)
+                    sendrer_messages(id=from_id, vk=vk,
+                                     text="Ты устроился на работу\nАванс в размере 20'000 вирт упал на счёт.")
+                    usr.money = usr.money + 20000
+                    db.commit()
+                except:
+                    what_r_u_say(upload, id=from_id, vk=vk)
 
-            if category == 'ДОМ':
-                home = db.query(Home).filter(Home.id == int(id))
-                if usr.money > home.cost:
-                    usr.home, usr.money = home.id, (usr.money - home.cost)
-                    sendrer_messages(id=from_id, vk=vk,
-                                     text=f'Ты приобрёл новый дом: {home.name}')
-                else:
-                    sendrer_messages(id=from_id, vk=vk,
-                                     text='Прости на твоём счету недостаточно денег')
+            if category == 'дом':
+                try:
+                    home = db.query(Home).filter(Home.id == int(id)).first()
+                    if usr.money > home.cost:
+                        usr.home_id, usr.money = home.id, (usr.money - home.cost)
+                        sendrer_messages(id=from_id, vk=vk,
+                                         text=f'Ты приобрёл новый дом: {home.name}')
+                        db.commit()
+                    else:
+                        sendrer_messages(id=from_id, vk=vk,
+                                         text='Прости на твоём счету недостаточно денег')
+                except:
+                    what_r_u_say(upload, id=from_id, vk=vk)
 
-            if category == 'МАШИНА':
-                car = db.query(Car).filter(Car.id == int(id))
-                if usr.money > Car.cost:
-                    usr.home, usr.money = Car.id, (usr.money - Car.cost)
-                    sendrer_messages(id=from_id, vk=vk,
-                                     text=f'Ты приобрёл новую машину: {car.name}')
-                else:
-                    sendrer_messages(id=from_id, vk=vk,
-                                     text='Прости на твоём счету недостаточно денег')
+            if category == 'машина':
+                try:
+                    car = db.query(Car).filter(Car.id == int(id)).first()
+                    if usr.money > car.cost:
+                        usr.car_id, usr.money = car.id, (usr.money - car.cost)
+                        sendrer_messages(id=from_id, vk=vk,
+                                         text=f'Ты приобрёл новую машину: {car.name}')
+                        db.commit()
+                    else:
+                        sendrer_messages(id=from_id, vk=vk,
+                                         text='Прости на твоём счету недостаточно денег')
+                except:
+                    what_r_u_say(upload, id=from_id, vk=vk)
 
-            if category == 'ПИТОМЕЦ':
-                animal = db.query(Animal).filter(Animal.id == int(id))
-                if usr.money > Animal.cost:
-                    usr.home, usr.money = Animal.id, (usr.money - Animal.cost)
-                    sendrer_messages(id=from_id, vk=vk,
-                                     text=f'Ты приобрёл нового питомца: {animal.name}')
-                else:
-                    sendrer_messages(id=from_id, vk=vk,
-                                     text='Прости на твоём счету недостаточно денег')
+            if category == 'питомец':
+                try:
+                    animal = db.query(Animal).filter(Animal.id == int(id)).first()
+                    if usr.money > Animal.cost:
+                        usr.animal_id, usr.money = animal.id, (usr.money - animal.cost)
+                        sendrer_messages(id=from_id, vk=vk,
+                                         text=f'Ты приобрёл нового питомца: {animal.name}')
+                        db.commit()
+                    else:
+                        sendrer_messages(id=from_id, vk=vk,
+                                         text='Прости на твоём счету недостаточно денег')
+                except:
+                    what_r_u_say(upload, id=from_id, vk=vk)
+
+
+def out_game(text, from_id, vk):
+    if text == 'выйти из игры':
+        sendrer_messages(text='Возращайся', id=from_id, vk=vk)
+        response = vk.users.get(user_ids=from_id, fields='first_name, last_name, city')
+        name = response[0]['first_name']
+        start('hi', response=response, name=name, from_id=from_id, vk=vk)
+
+
+def settings(text, from_id, vk):
+    if text == 'настройки':
+        bol = chek_player(from_id, vk)
+        if bol:
+            sendrer_messages(vk=vk, id=from_id,
+                             text='Ты защел во вкладку настройки. Тут ты можешь поменять свой ник в игре.')
+            sendrer_messages(vk=vk, id=from_id,
+                             text='Чтобы поменять имя, напиши: измени моё имя - ..., и пишешь какое имя хочешь иметь\nС фамилией аналогично')
+    if text.startswith('измени моё имя - '):
+        bol = chek_player(from_id, vk)
+        if bol:
+            txt = text.split()
+            name = txt[-1]
+            change_data_player(from_id, name=name)
+            sendrer_messages(vk=vk, id=from_id,
+                             text=f'Отлично ты поменял своё имя, теперь твоё имя: {name}')
+    if text.startswith('измени мою фамилию - '):
+        bol = chek_player(from_id, vk)
+        if bol:
+            txt = text.split()
+            surname = txt[-1]
+            change_data_player(from_id, surname=surname)
+            sendrer_messages(vk=vk, id=from_id,
+                             text=f'Отлично ты поменял свою фамилию, теперь твоя фамилия: {surname}')
 
 
 def main():
@@ -381,13 +446,16 @@ def main():
                                                                                                                time.asctime()))
 
             # действия
-            what_r_u_say(text, from_id, vk, upload=upload)
+            if text == '':
+                what_r_u_say(upload, from_id, vk)
             start(text, response, name, from_id, vk)
             date_time(text, from_id, vk)
             del_keyb(text=text, from_id=from_id, keyb=keyboard, vk=vk)
             game(text=text, from_id=from_id, vk=vk)
             if text.startswith('/'):
-                commands(text[1:], from_id, vk)
+                commands(text[1:], from_id, vk, upload)
+            out_game(text, from_id, vk)
+            settings(text, from_id, vk)
 
 
 if __name__ == '__main__':
